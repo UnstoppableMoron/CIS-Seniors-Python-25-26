29e5c0c1085fc3fd9d2e844c0489284f9199e47e
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-16 13:53:13 +0000

    Sherma my boy

---
+++ printExpressions.py
@@ -0,0 +1,15 @@
+"""
+Class: CIS Seniors 25-26
+Name: Owen Baker
+Assignment: Printing Expressions
+"""
+
+print("Hello, my name is Owen.")
+
+print("My favorite song!")
+print("-----------------")
+print()
+print("Red Doors please open up to me!")
+print("I promise I mean no harm to thee!")
+print("There's a thousand reasons to do so you'll find.")
+print("And if you do it'd be so kind!")
