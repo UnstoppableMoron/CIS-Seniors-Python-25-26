c1d8d1e8611f184100700a93cf1581f735e40882
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-18 13:33:51 +0000

    dreamVacation

---
+++ dreamVacation.py
@@ -0,0 +1,18 @@
+'''
+File Name: dreamVacation.py
+Name: Owen Baker
+Course Name: Computer and Information Science
+Date: 9/18/25
+'''
+
+location = "Iceland"
+cost = "$4000"
+duration = "two weeks"
+activities = "hanging out, just kinda being there"
+travelCompanions = "various friends, probably"
+
+print("I would like to go to " + location + ".")
+print("It should cost around " + cost + ".")
+print("I plan to be there for " + duration + ".")
+print("I will be doing various things including: " + activities + ".")
+print("I will be going with " + travelCompanions + ".")
---
+++ morePyVars.py
@@ -0,0 +1,35 @@
+'''
+The Doc String
+Notes About Python Variables
+Name: Owen Baker
+Date: 9/18/25
+'''
+print("\n\n\n")
+# Variable Assignment
+firstName = "Owen"
+lastName = "Baker"
+gradeLevel = 12
+favoriteSubject = "Computer Science"
+yearsInSchool = "too many"
+
+print("My name is " + firstName + " " + lastName + ".")
+print("I am in" , gradeLevel , "th grade.")
+print("My favorite subject is " + favoriteSubject + ".")
+print("I have been in school for " + yearsInSchool + "years.")
+
+print("\n\n\n")
+
+print("Mathematical Calculations:")
+
+# Storing mathematical values
+length = 15
+width = 8
+height = 12
+
+# Calculate area and volume and store them in variables
+area = length * width
+volume = length * width * height
+
+# Print out the values
+print("The area of our structure is:", area)
+print("The volume of our structure is:", volume)
