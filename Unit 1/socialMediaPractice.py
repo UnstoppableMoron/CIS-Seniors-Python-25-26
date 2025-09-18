16b258c0ec9e38450d5067aae0924f2fa55cf9a9
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-18 14:36:50 +0000

    Social Media Example

---
+++ Unit 1/socialMediaPractice.py
@@ -0,0 +1,28 @@
+'''
+File Name: socialMediaPractice.py
+Name: Owen Baker
+Course Name: Computer and Information Science
+Date: 9/18/25
+'''
+
+username = "UnstoppableMoron"
+bio = "the eepiest sleeper"
+favorite_quote = "Bapanada"
+followers = 2
+following = 42
+posts = 1
+average_likes_per_post = 6
+engagement_rate = 2
+verified_account = False
+private_profile = False
+
+print("Username: " + username)
+print("Bio: " + bio)
+print("Favorite Quote: " + favorite_quote)
+print("Followers:" , followers)
+print("Following:" , following)
+print("Posts:" , posts)
+print("Average Likes Per Post:" , average_likes_per_post)
+print("Engagement Rate:" , engagement_rate)
+print("Verified:," , verified_account)
+print("Private:" , private_profile)
