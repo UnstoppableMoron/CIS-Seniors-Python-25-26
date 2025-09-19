a0422f44a54d2eea480476d25a46fbd248ddb175
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-19 13:31:13 +0000

    great

--- Unit 1/input.py
+++ Unit 1/input.py
@@ -8,3 +8,5 @@ Date: 9/19/2025
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 age = input("How old are you? ")
+math = input("What is pi? ")
+favorite = input("Is this your favorite class? ")
