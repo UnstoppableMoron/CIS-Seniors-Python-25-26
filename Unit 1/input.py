1d16dce1bc88de23f0f55e831ad9dfa54ab8caca
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-19 13:27:32 +0000

    damn battery

---
+++ Unit 1/input.py
@@ -0,0 +1,10 @@
+'''
+Filename: input.py
+Author: Owen Baker
+Class: CIS Seniors
+Date: 9/19/2025
+'''
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("How old are you? ")
