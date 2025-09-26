8d3a65b95bb3bbe5f2e31c32583d9d8fef5f5987
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-09-26 14:10:18 +0000

    uhsgdh

--- Unit 1/Input/quizScorer.py
+++ Unit 1/Input/quizScorer.py
@@ -18,21 +18,36 @@ print("=== QUIZ SCORER ===")
 print("This program will score a 5-10 question true/false quiz.")
 studentName = input("What is the student's name? ")
 questions = int(input("How many quiz questions are there? (5-10) "))
+
 totalScore = 0
+counter = 0
+totalCorrect = 0
 
 lst = []
 print("Enter student's answers, 1 for true, 0 for false. ")
-for question in range(1, questions + 1):
-    answer = int(input("Question " + str(question) + ": "))
+for counter in range(1, questions + 1):
+    answer = list((input("Question " + str(counter) + ": ")))
     lst.append(answer)
 
 correctLst = []
 print("Enter the correct answers., 1 for true, 0 for false. ")
-for question in range(1, questions + 1):
-    correctAnswer = int(input("Question " + str(question) + ": "))
+for counter in range(1, questions + 1):
+    correctAnswer = list(input("Question " + str(counter) + ": "))
     correctLst.append(correctAnswer)
 
-if answer == correctAnswer:
-    print("Question " + str(question) + " Correct")
+for counter in range(1, (questions + 1)):
+    if lst[counter - 1] == correctLst[counter - 1]:
+        print("Question " + str(counter) + ":" + " Correct")
+        totalCorrect += 1 
+    elif lst[counter - 1] != correctLst[counter - 1]:
+        print("Question " + str(counter) + ":" + " Incorrect")
+
+print("\n=== Quiz results for: " + studentName + " ===")
+print("Correct answers:" , totalCorrect)
+totalCorrect = (totalCorrect / questions) * 100
+print("Quiz percentage:" , totalCorrect , "%")
+
+if totalCorrect >= 70: 
+    print("Final status: PASS")
 else:
-    print("Question " + str(question) + " Incorrect")
+    print("Final status: FAIL")
